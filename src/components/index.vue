9e555ec1ac0d2e0efa78d6528d353f3ca947e333
Changpeng Duan <838560574@qq.com>
2017-07-23 17:07:03 +0800

    6

--- src/components/index.vue
+++ src/components/index.vue
@@ -2,14 +2,18 @@
     <div class="index">
         <h1>欢迎来到狼人杀助手demo</h1>
         <p>本局人数</p>
-        <v-select v-model="initValue" dropdown-class-name="aaaaa" :dropdown-z-index="9898"  >
-            <v-option value="9">9</v-option>
-            <v-option value="10">10</v-option>
-            <v-option value="11">11</v-option>
-            <v-option value="12">12</v-option>
-        </v-select>
+        <!--<v-select v-model="initValue" dropdown-class-name="aaaaa" :dropdown-z-index="9898"  >-->
+            <!--<v-option value="9">9</v-option>-->
+        <!--</v-select>-->
         <br>
-        <v-button large primary value="确定" ></v-button>
+        <v-button large primary value="确定" @click.native="show = true"></v-button>
+
+      <modal :show="show" ref="dialog" @close="show = false">
+        <div slot="modal-body" class="atui-modal-body">
+          <textarea></textarea>
+        </div>
+      </modal>
+
     </div>
 </template>
 
@@ -18,10 +22,16 @@
     import atui from 'atui'
     export default {
         name: 'index',
+        methods:{
+          getPeople:function () {
+            console.log(123);
+          }
+        },
         components: {
             vButton: atui.Button,
-            vSelect: atui.Select,
-            vOption: atui.Option,
+            vModal: atui.Modal,
+//            vSelect: atui.Select,
+//            vOption: atui.Option,
         }
     }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/components/index'
+import desk from '@/components/desk'
 Vue.use(Router)
 
 export default new Router({
+  mode:'history',
   routes: [
     {
       path: '/',
       name: 'index',
       component: index
+    },
+    {
+      path: 'desk',
+      name: 'desk',
+      component: desk
     }
   ]
 })
